a6a7c2b5c68c36924051db1039b5fb98b91b9fa1
norume <norume@9d289d9b-4bc5-ceb4-995e-c1689a017bcc>
2003-10-31 19:22:08 +0000

    New command names.

--- trunk/iocBoot/ioctestGpib/st.cmd
+++ trunk/iocBoot/ioctestGpib/st.cmd
@@ -3,16 +3,17 @@ testGpib_registerRecordDeviceDriver(pdbbase)
 
 dbLoadRecords("../../db/testGpib.db","name=gpibTest,L=0,A=3")
 
+var vxi11Debug 3
+
 #The following two commands are for the E2050
 #E2050Reboot("164.54.8.227")
-#vxi11Config("L0","164.54.8.227",0,0.0,"hpib",0)
+#vxi11Configure("L0","164.54.8.227",0,0.0,"hpib",0)
 
 #The following two commands are for the E5810
 #E5810Reboot("164.54.8.129",0)
-vxi11Config("L0","164.54.8.129",0,0.0,"gpib0",0)
+vxi11Configure("L0","164.54.8.129",0,0.0,"gpib0",0)
 
 #The following command is for an ethernet TSD3014B scope
-#vxi11Config("L0","164.54.8.137",0,0.0,"inst0",1)
+#vxi11Configure("L0","164.54.8.137",0,0.0,"inst0",0)
 
-#var vxi11Debug 3
 iocInit()
