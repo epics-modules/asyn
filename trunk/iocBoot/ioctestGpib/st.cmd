29fddfccf2a833e9f7e73b5b6b8a5e2ac9a2cb2e
mrk <mrk@9d289d9b-4bc5-ceb4-995e-c1689a017bcc>
2003-11-14 16:58:46 +0000

    so that I can tag

--- trunk/iocBoot/ioctestGpib/st.cmd
+++ trunk/iocBoot/ioctestGpib/st.cmd
@@ -3,15 +3,15 @@ testGpib_registerRecordDeviceDriver(pdbbase)
 
 dbLoadRecords("../../db/testGpib.db","name=gpibTest,L=0,A=3")
 
-var vxi11Debug 3
+#var vxi11Debug 3
 
 #The following two commands are for the E2050
 #E2050Reboot("164.54.8.227")
-#vxi11Configure("L0","164.54.8.227",0,0.0,"hpib",0)
+vxi11Configure("L0","164.54.8.227",0,0.0,"hpib",0)
 
 #The following two commands are for the E5810
 #E5810Reboot("164.54.8.129",0)
-vxi11Configure("L0","164.54.8.129",0,0.0,"gpib0",0)
+#vxi11Configure("L0","164.54.8.129",0,0.0,"gpib0",0)
 
 #The following command is for an ethernet TSD3014B scope
 #vxi11Configure("L0","164.54.8.137",0,0.0,"inst0",0)
