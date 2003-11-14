1b6bce2093db1cc1539e1316f601cb8be8740d61
mrk <mrk@9d289d9b-4bc5-ceb4-995e-c1689a017bcc>
2003-11-14 19:56:13 +0000

    add test for vxWorks

---
+++ trunk/iocBoot/ioctestGpibVx/st.cmd
@@ -0,0 +1,29 @@
+# Following must be added for many board support packages
+#cd <full path to target bin directory>
+#cd "src/EPICS/modules/soft/asyn/iocBoot/ioctestGpibVx"
+
+< cdCommands
+
+cd topbin
+ld < testGpibVx.munch
+cd startup
+
+dbLoadDatabase("../../dbd/testGpibVx.dbd")
+testGpibVx_registerRecordDeviceDriver(pdbbase)
+
+dbLoadRecords("../../db/testGpib.db","name=gpibTest,L=0,A=3")
+
+vxi11Debug = 10
+
+#The following command is for the E2050
+#E2050Reboot("164.54.8.227")
+vxi11Configure("L0","164.54.8.227",0,0.0,"hpib",0)
+
+#The following two commands are for the E5810
+#E5810Reboot("164.54.8.129",0)
+#vxi11Configure("L0","164.54.8.129",0,0.0,"gpib0",0)
+
+#The following command is for an ethernet TSD3014B scope
+#vxi11Configure("L0","164.54.8.137",0,0.0,"inst0",0)
+
+iocInit()
