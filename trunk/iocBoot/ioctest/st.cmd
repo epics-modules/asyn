d495b3d1390223584f91e63dead640c164e67c15
mrk <mrk@9d289d9b-4bc5-ceb4-995e-c1689a017bcc>
2003-11-21 21:04:07 +0000

    changes for trace facility

--- trunk/iocBoot/ioctest/st.cmd
+++ trunk/iocBoot/ioctest/st.cmd
@@ -2,8 +2,3 @@ test_registerRecordDeviceDriver(pdbbase)
 
 echoDriverInit(echo,1.0)
 processModuleInit(processModule,echo,1)
-testEcho(echo,0,oneA0,5,10.0)
-testEcho(echo,0,twoA0,3,10.0)
-testEcho(echo,1,oneA1,5,10.0)
-testEcho(echo,1,twoA1,3,10.0)
-asynReport("",3)
