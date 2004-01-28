d108c9024cae1d9c6f8104892b4b5cab8ed9be7e
norume <norume@9d289d9b-4bc5-ceb4-995e-c1689a017bcc>
2004-01-28 19:55:42 +0000

    Make error message match code.

--- trunk/asyn/vxi11/E2050Reboot.c
+++ trunk/asyn/vxi11/E2050Reboot.c
@@ -53,7 +53,7 @@ int E2050Reboot(char * inetAddr)
         return(-1);
     }
     nbytes = send(fd,"reboot\ny\n",9,0);
-    if(nbytes!=9) printf("nbytes %d expected 7\n",nbytes);
+    if(nbytes!=9) printf("nbytes %d expected 9\n",nbytes);
     close(fd);
     epicsThreadSleep(10.0);
     return(0);
