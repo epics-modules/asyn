e7598944762ecccec982032c7cd5e9f22f3e7988
mrk <mrk@9d289d9b-4bc5-ceb4-995e-c1689a017bcc>
2004-03-11 14:57:24 +0000

    changes for connection management and interpose

--- trunk/iocBoot/ioctestGpib/st.cmd
+++ trunk/iocBoot/ioctestGpib/st.cmd
@@ -2,19 +2,19 @@ dbLoadDatabase("../../dbd/testGpib.dbd")
 testGpib_registerRecordDeviceDriver(pdbbase)
 
 dbLoadRecords("../../db/testGpib.db","name=gpibTest,L=0,A=3")
-dbLoadRecords("../../db/asynTrace.db","ioc=gpibTest,port=L0,addr=3")
+dbLoadRecords("../../db/asynGeneric.db","ioc=gpibTest")
 
 
 #The following two commands are for the E2050
 #E2050Reboot("164.54.8.227")
-#vxi11Configure("L0","164.54.8.227",0,0.0,"hpib",0)
+#vxi11Configure("L0","164.54.8.227",0,0.0,"hpib",0,0)
 
 #The following two commands are for the E5810
 E5810Reboot("164.54.8.129",0)
-vxi11Configure("L0","164.54.8.129",0,0.0,"gpib0",0)
+vxi11Configure("L0","164.54.8.129",0,0.0,"gpib0",0,0)
 
 #The following command is for an ethernet TSD3014B scope
-#vxi11Configure("L0","164.54.8.137",0,0.0,"inst0",0)
+#vxi11Configure("L0","164.54.8.137",0,0.0,"inst0",0,0)
 
 asynSetTraceMask("L0",3,0xff)
 asynSetTraceIOMask("L0",3,0x2)
--- trunk/iocBoot/ioctestGpibSerialVx/st.cmd
+++ trunk/iocBoot/ioctestGpibSerialVx/st.cmd
@@ -12,17 +12,17 @@ dbLoadDatabase("../../dbd/testGpibSerialVx.dbd")
 testGpibSerialVx_registerRecordDeviceDriver(pdbbase)
 
 dbLoadRecords("../../db/testGpib.db","name=gpibTest,L=0,A=3")
-dbLoadRecords("../../db/asynTrace.db","ioc=gpibTest,port=L0,addr=3")
+dbLoadRecords("../../db/asynGeneric.db","ioc=gpibTest")
 
 #The following command is for a serial line terminal concentrator
-#drvGenericSerialConfigure("L0","164.54.9.93:4003",0,0)
+#drvGenericSerialConfigure("L0","164.54.9.93:4003",0,0,0)
 
 #The following commands are for a Greenspring octal UART and VIP616-01 carrier
 ipacAddVIPC616_01("0x6000,B0000000")
 tyGSOctalDrv(1)
 tyGSOctalModuleInit("RS232", 0x80, 0, 0)
 tyGSOctalDevCreate("/tyGS/0/0",0,0,1000,1000)
-drvGenericSerialConfigure("L0","/tyGS/0/0",0,0)
+drvGenericSerialConfigure("L0","/tyGS/0/0",0,0,0)
 asynSetPortOption("L0", "baud", "9600")
 asynSetPortOption("L0", "bits", "8")
 asynSetPortOption("L0", "parity", "none")
