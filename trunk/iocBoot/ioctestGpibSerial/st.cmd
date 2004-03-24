c6efcbb2dfc44356ada23b94ff6c51212b809594
norume <norume@9d289d9b-4bc5-ceb4-995e-c1689a017bcc>
2004-03-24 20:01:18 +0000

    Add 'autoConnect' parameter. Use 'single-connection' address value for debug.

--- trunk/iocBoot/ioctestGpibSerial/st.cmd
+++ trunk/iocBoot/ioctestGpibSerial/st.cmd
@@ -3,14 +3,14 @@
 dbLoadDatabase("../../dbd/testGpibSerial.dbd")
 testGpibSerial_registerRecordDeviceDriver(pdbbase)
 
-dbLoadRecords("../../db/testGpib.db","name=gpibTest,L=0,A=3")
+dbLoadRecords("../../db/testGpib.db","name=gpibTest,L=0,A=0")
 dbLoadRecords("../../db/asynGeneric.db","ioc=gpibTest")
 
 #The following command is for a serial line terminal concentrator
-drvGenericSerialConfigure("L0","164.54.9.93:4003",0,0,0)
+drvGenericSerialConfigure("L0","164.54.9.93:4003",1,0)
 
 #The following commands are for a local serial line
-#drvGenericSerialConfigure("L0","/dev/cua/a",0,0)
+#drvGenericSerialConfigure("L0","/dev/cua/a",1,0)
 #asynSetPortOption("L0", "baud", "9600")
 #asynSetPortOption("L0", "bits", "8")
 #asynSetPortOption("L0", "parity", "none")
@@ -18,8 +18,7 @@ drvGenericSerialConfigure("L0","164.54.9.93:4003",0,0,0)
 #asynSetPortOption("L0", "clocal", "Y")
 #asynSetPortOption("L0", "crtscts", "N")
 
-
-asynSetTraceMask("L0",3,0xff)
-asynSetTraceIOMask("L0",3,0x2)
+asynSetTraceMask("L0",-1,0xff)
+asynSetTraceIOMask("L0",-1,0x2)
 
 iocInit()
