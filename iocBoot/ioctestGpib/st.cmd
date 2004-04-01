63f63a908aae0c2feec38b86cf49ac366fc00988
Marty Kraimer <mrkraimer@comcast.net>
2004-04-01 16:49:07 +0000

    final settingd for release 2-1

--- iocBoot/ioctestGpib/st.cmd
+++ iocBoot/ioctestGpib/st.cmd
@@ -3,7 +3,7 @@ testGpib_registerRecordDeviceDriver(pdbbase)
 
 dbLoadRecords("../../db/testGpib.db","name=gpibTest,L=0,A=3")
 dbLoadRecords("../../db/asynGeneric.db","ioc=gpibTest")
-dbLoadRecords("../../db/asynOctetRecord.db","P=mrk,R=Octet,PORT=A,ADDR=0,OMAX=0,IMAX=0")
+dbLoadRecords("../../db/asynOctetRecord.db","P=mrk,R=Octet,PORT=L0,ADDR=3,OMAX=0,IMAX=0")
 
 
 #The following two commands are for the E2050
@@ -17,8 +17,8 @@ vxi11Configure("L0","164.54.8.129",0,0.0,"gpib0",0,0)
 #The following command is for an ethernet TSD3014B scope
 #vxi11Configure("L0","164.54.8.137",0,0.0,"inst0",0,0)
 
-asynSetTraceMask("L0",3,0xff)
-asynSetTraceIOMask("L0",3,0x2)
-asynSetTraceMask("L0",-1,0xff)
+#asynSetTraceMask("L0",3,0xff)
+#asynSetTraceIOMask("L0",3,0x2)
+#asynSetTraceMask("L0",-1,0xff)
 
 iocInit()
