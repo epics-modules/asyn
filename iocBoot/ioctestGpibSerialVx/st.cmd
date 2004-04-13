fdc16338269a48bcb73ebf4eae328af7a66b09e2
norume <norume@9d289d9b-4bc5-ceb4-995e-c1689a017bcc>
2004-04-13 18:51:10 +0000

    New serial port configure/options.

--- iocBoot/ioctestGpibSerialVx/st.cmd
+++ iocBoot/ioctestGpibSerialVx/st.cmd
@@ -15,19 +15,19 @@ dbLoadRecords("../../db/testGpib.db","name=gpibTest,L=0,A=3")
 dbLoadRecords("../../db/asynRecord.db","P=asyn,R=Test,PORT=L0,ADDR=3,OMAX=0,IMAX=0")
 
 #The following command is for a serial line terminal concentrator
-drvGenericSerialConfigure("L0","164.54.9.90:4004",0,0)
+drvAsynTCPPortConfigure("L0","164.54.9.90:4004",0,0)
 
 #The following commands are for a Greenspring octal UART and VIP616-01 carrier
 #ipacAddVIPC616_01("0x6000,B0000000")
 #tyGSOctalDrv(1)
 #tyGSOctalModuleInit("RS232", 0x80, 0, 0)
 #tyGSOctalDevCreate("/tyGS/0/0",0,0,1000,1000)
-#drvGenericSerialConfigure("L0","/tyGS/0/0",0,0)
-#asynSetPortOption("L0", "baud", "9600")
-#asynSetPortOption("L0", "bits", "8")
-#asynSetPortOption("L0", "parity", "none")
-#asynSetPortOption("L0", "stop", "1")
-#asynSetPortOption("L0", "clocal", "Y")
-#asynSetPortOption("L0", "crtscts", "N")
+#drvAsynSerialPortConfigure("L0","/tyGS/0/0",0,0,0)
+#asynSetOption("L0", 0, "baud", "9600")
+#asynSetOption("L0", 0, "bits", "8")
+#asynSetOption("L0", 0, "parity", "none")
+#asynSetOption("L0", 0, "stop", "1")
+#asynSetOption("L0", 0, "clocal", "Y")
+#asynSetOption("L0", 0, "crtscts", "N")
 
 iocInit()
