4f61f19c34a7774a10550814d563e2fc8cdda911
norume <norume@9d289d9b-4bc5-ceb4-995e-c1689a017bcc>
2004-04-21 17:42:40 +0000

    Changes to match latest ASYN/BASE release.

--- trunk/documentation/HowToDoSerial/AB300/iocBoot/iocAB300/st.cmd
+++ trunk/documentation/HowToDoSerial/AB300/iocBoot/iocAB300/st.cmd
@@ -2,19 +2,22 @@ dbLoadDatabase("../../dbd/AB300.dbd",0,0)
 AB300_registerRecordDeviceDriver(pdbbase) 
 dbLoadRecords("../../db/AB300.db","user=AB300")
 
+# Create a diagnostic asynRecord
+#dbLoadRecords("../../db/asynRecord.db","P=AB300,R=Test,PORT=L0,ADDR=0,IMAX=0,OMAX=0")
+
 #The following command is for a serial line terminal concentrator
-drvGenericSerialConfigure("L0","164.54.3.137:4001",1,0)
+drvAsynTCPPortConfigure("L0","164.54.3.137:4001",0,0,0)
 
 #The following commands are for a local serial line
-#drvGenericSerialConfigure("L0","/dev/ttyS0",0,0)
-#asynSetPortOption("L0", "baud", "9600")
-#asynSetPortOption("L0", "bits", "8")
-#asynSetPortOption("L0", "parity", "none")
-#asynSetPortOption("L0", "stop", "1")
-#asynSetPortOption("L0", "clocal", "Y")
-#asynSetPortOption("L0", "crtscts", "N")
+#drvAsynSerialPortConfigure("L0","/dev/ttyS0",0,0,0)
+#asynSetOption("L0", -1, "baud", "9600")
+#asynSetOption("L0", -1, "bits", "8")
+#asynSetOption("L0", -1, "parity", "none")
+#asynSetOption("L0", -1, "stop", "1")
+#asynSetOption("L0", -1, "clocal", "Y")
+#asynSetOption("L0", -1, "crtscts", "N")
 
-asynSetTraceMask("L0",-1,0xff)
+asynSetTraceMask("L0",-1,0x9)
 asynSetTraceIOMask("L0",-1,0x2)
 
 iocInit()
