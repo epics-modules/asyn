013d0fbdd77d1007675b8bfd488fd33c6c4f04fc
Mark Rivers <MarkRivers@users.noreply.github.com>
2004-07-26 14:10:55 +0000

    Added structure name

--- asyn/asynMisc/asynInt32Callback.h
+++ asyn/asynMisc/asynInt32Callback.h
@@ -12,14 +12,17 @@
 
 #define asynInt32CallbackType "asynInt32Callback"
 
-typedef void (*asynInt32Clbk)(void *drvPvt, epicsInt32 readback);
+typedef void (*asynInt32DataCallback)(void *drvPvt, epicsInt32 data);
+typedef void (*asynInt32IntervalCallback)(void *drvPvt, double seconds);
 
-typedef struct {
+typedef struct asynInt32Callback {
     double     (*setCallbackInterval)(void *drvPvt, asynUser *pasynUser,
                                       double seconds);
     double     (*getCallbackInterval)(void *drvPvt, asynUser *pasynUser);
-    asynStatus (*registerCallback)   (void *drvPvt, asynUser *pasynUser,
-                                      asynInt32Clbk callback, void *pvt);
+    asynStatus (*registerCallbacks)(void *drvPvt, asynUser *pasynUser,
+                                    asynInt32DataCallback dataCallback, 
+                                    asynInt32IntervalCallback intervalCallback,
+                                    void *pvt);
 } asynInt32Callback;
 
 #endif /* asynInt32CallbackH*/
