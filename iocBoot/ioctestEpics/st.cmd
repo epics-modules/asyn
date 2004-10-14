12e3548ea94086861d354e90bf3892d5be53745c
Marty Kraimer <mrkraimer@comcast.net>
2004-10-14 19:30:28 +0000

    major changes

--- iocBoot/ioctestEpics/st.cmd
+++ iocBoot/ioctestEpics/st.cmd
@@ -1,12 +1,16 @@
 dbLoadDatabase("../../dbd/testEpics.dbd")
 testEpics_registerRecordDeviceDriver(pdbbase)
 
-cacheInt32DriverInit("cacheA",0.05,0)
-interruptInt32DriverInit("intA",0.1,0)
-echoDriverInit("echo",0.1,0,0)
-dbLoadRecords("../../db/asynRecord.db","P=asyn,R=Record_PA_A0,PORT=cacheA,ADDR=0,OMAX=0,IMAX=0")
-dbLoadRecords("../../db/devInt32.db","PORT=cacheA,ADDR=0")
-dbLoadRecords("../../db/devInt32.db","PORT=cacheA,ADDR=1")
-dbLoadRecords("../../db/devInt32Interrupt.db","PORT=intA")
-dbLoadRecords("../../db/devOctet.db","PORT=echo,ADDR=0")
+int32DriverInit("cacheInt32",0.0,0,10)
+int32DriverInit("interruptInt32",0.1,0,10)
+dbLoadRecords("../../db/devInt32.db")
+
+uint32DigitalDriverInit("digital")
+dbLoadRecords("../../db/devDigital.db")
+
+#echoDriverInit("echo",0.1,0,0)
+#dbLoadRecords("../../db/devOctet.db","PORT=echo,ADDR=0")
+
+dbLoadRecords("../../db/asynRecord.db","P=asyn,R=Record,PORT=digital,ADDR=0,OMAX=0,IMAX=0")
+
 iocInit()
