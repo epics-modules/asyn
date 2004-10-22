0b325c7e90f4bac346212e71530d95761d17ae66
Marty Kraimer <mrkraimer@comcast.net>
2004-10-22 16:17:08 +0000

    lastest version

--- iocBoot/ioctestEpics/st.cmd
+++ iocBoot/ioctestEpics/st.cmd
@@ -1,15 +1,14 @@
 dbLoadDatabase("../../dbd/testEpics.dbd")
 testEpics_registerRecordDeviceDriver(pdbbase)
 
-int32DriverInit("cacheInt32",0.0,0,10)
-int32DriverInit("interruptInt32",0.1,0,10)
+int32DriverInit("int32",-32768,32767)
 dbLoadRecords("../../db/devInt32.db")
 
 uint32DigitalDriverInit("digital")
 dbLoadRecords("../../db/devDigital.db")
 
-#echoDriverInit("echo",0.1,0,0)
-#dbLoadRecords("../../db/devOctet.db","PORT=echo,ADDR=0")
+echoDriverInit("echo",0.1,0,0)
+dbLoadRecords("../../db/devOctet.db","PORT=echo,ADDR=0")
 
 dbLoadRecords("../../db/asynRecord.db","P=asyn,R=Record,PORT=digital,ADDR=0,OMAX=0,IMAX=0")
 
