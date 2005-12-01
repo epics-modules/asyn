87bb55daf8162e9247e75b863cd47fb99cca2d52
rivers <rivers@9d289d9b-4bc5-ceb4-995e-c1689a017bcc>
2005-12-01 15:52:46 +0000

    Removed epicsShareAPI from device support functions to work on WIN32

--- trunk/asyn/devGpib/devCommonGpib.h
+++ trunk/asyn/devGpib/devCommonGpib.h
@@ -24,38 +24,34 @@
 extern "C" {
 #endif  /* __cplusplus */
 
-epicsShareFunc long epicsShareAPI devGpib_initAi(struct aiRecord * pai);
-epicsShareFunc long epicsShareAPI devGpib_readAi(struct aiRecord * pai);
-epicsShareFunc long epicsShareAPI devGpib_initAo(struct aoRecord * pao);
-epicsShareFunc long epicsShareAPI devGpib_writeAo(struct aoRecord * pao);
-epicsShareFunc long epicsShareAPI devGpib_initBi(struct biRecord * pbi);
-epicsShareFunc long epicsShareAPI devGpib_readBi(struct biRecord * pbi);
-epicsShareFunc long epicsShareAPI devGpib_initBo(struct boRecord * pbo);
-epicsShareFunc long epicsShareAPI devGpib_writeBo(struct boRecord * pbo);
-epicsShareFunc long epicsShareAPI devGpib_initEv(struct eventRecord * pev);
-epicsShareFunc long epicsShareAPI devGpib_readEv(struct eventRecord * pev);
-epicsShareFunc long epicsShareAPI devGpib_initLi(struct longinRecord * pli);
-epicsShareFunc long epicsShareAPI devGpib_readLi(struct longinRecord * pli);
-epicsShareFunc long epicsShareAPI devGpib_initLo(struct longoutRecord * plo);
-epicsShareFunc long epicsShareAPI devGpib_writeLo(struct longoutRecord * plo);
-epicsShareFunc long epicsShareAPI devGpib_initMbbi(struct mbbiRecord * pmbbi);
-epicsShareFunc long epicsShareAPI devGpib_readMbbi(struct mbbiRecord * pmbbi);
-epicsShareFunc long epicsShareAPI 
-    devGpib_initMbbiDirect(struct mbbiDirectRecord * pmbbiDirect);
-epicsShareFunc long epicsShareAPI 
-    devGpib_readMbbiDirect(struct mbbiDirectRecord * pmbbiDirect);
-epicsShareFunc long epicsShareAPI devGpib_initMbbo(struct mbboRecord * pmbbo);
-epicsShareFunc long epicsShareAPI devGpib_writeMbbo(struct mbboRecord * pmbbo);
-epicsShareFunc long epicsShareAPI 
-    devGpib_initMbboDirect(struct mbboDirectRecord * pmbboDirect);
-epicsShareFunc long epicsShareAPI 
-    devGpib_writeMbboDirect(struct mbboDirectRecord * pmbboDirect);
-epicsShareFunc long epicsShareAPI devGpib_initSi(struct stringinRecord * psi);
-epicsShareFunc long epicsShareAPI devGpib_readSi(struct stringinRecord * psi);
-epicsShareFunc long epicsShareAPI devGpib_initSo(struct stringoutRecord * pso);
-epicsShareFunc long epicsShareAPI devGpib_writeSo(struct stringoutRecord * pso);
-epicsShareFunc long epicsShareAPI devGpib_initWf(struct waveformRecord * pwf);
-epicsShareFunc long epicsShareAPI devGpib_readWf(struct waveformRecord * pwf);
+epicsShareFunc long  devGpib_initAi(struct aiRecord * pai);
+epicsShareFunc long  devGpib_readAi(struct aiRecord * pai);
+epicsShareFunc long  devGpib_initAo(struct aoRecord * pao);
+epicsShareFunc long  devGpib_writeAo(struct aoRecord * pao);
+epicsShareFunc long  devGpib_initBi(struct biRecord * pbi);
+epicsShareFunc long  devGpib_readBi(struct biRecord * pbi);
+epicsShareFunc long  devGpib_initBo(struct boRecord * pbo);
+epicsShareFunc long  devGpib_writeBo(struct boRecord * pbo);
+epicsShareFunc long  devGpib_initEv(struct eventRecord * pev);
+epicsShareFunc long  devGpib_readEv(struct eventRecord * pev);
+epicsShareFunc long  devGpib_initLi(struct longinRecord * pli);
+epicsShareFunc long  devGpib_readLi(struct longinRecord * pli);
+epicsShareFunc long  devGpib_initLo(struct longoutRecord * plo);
+epicsShareFunc long  devGpib_writeLo(struct longoutRecord * plo);
+epicsShareFunc long  devGpib_initMbbi(struct mbbiRecord * pmbbi);
+epicsShareFunc long  devGpib_readMbbi(struct mbbiRecord * pmbbi);
+epicsShareFunc long  devGpib_initMbbiDirect(struct mbbiDirectRecord * pmbbiDirect);
+epicsShareFunc long  devGpib_readMbbiDirect(struct mbbiDirectRecord * pmbbiDirect);
+epicsShareFunc long  devGpib_initMbbo(struct mbboRecord * pmbbo);
+epicsShareFunc long  devGpib_writeMbbo(struct mbboRecord * pmbbo);
+epicsShareFunc long  devGpib_initMbboDirect(struct mbboDirectRecord * pmbboDirect);
+epicsShareFunc long  devGpib_writeMbboDirect(struct mbboDirectRecord * pmbboDirect);
+epicsShareFunc long  devGpib_initSi(struct stringinRecord * psi);
+epicsShareFunc long  devGpib_readSi(struct stringinRecord * psi);
+epicsShareFunc long  devGpib_initSo(struct stringoutRecord * pso);
+epicsShareFunc long  devGpib_writeSo(struct stringoutRecord * pso);
+epicsShareFunc long  devGpib_initWf(struct waveformRecord * pwf);
+epicsShareFunc long  devGpib_readWf(struct waveformRecord * pwf);
 
 
 #ifdef __cplusplus
