b8dc8943ce4334d0ec65413aa1c701b2502e60d6
Mark Rivers <MarkRivers@users.noreply.github.com>
2005-12-07 23:54:03 +0000

    New file, temporary until these functions are added into epicsString in base

---
+++ asyn/miscellaneous/epicsStrtok_r.h
@@ -0,0 +1,9 @@
+#ifdef __cplusplus
+extern "C" {
+#endif  /* __cplusplus */
+
+epicsShareFunc char * epicsStrtok_r(char *s, const char *delim, char **lasts);
+
+#ifdef __cplusplus
+}
+#endif  /* __cplusplus */
