4f78bb6cfed5bb2fe65c48950a6cc458ed704502
Mark Rivers <MarkRivers@users.noreply.github.com>
2005-12-14 15:10:26 +0000

    No longer needed, moved to epicsString in base 3.14.9

--- asyn/miscellaneous/epicsStrtok_r.h
+++
@@ -1,9 +0,0 @@
-#ifdef __cplusplus
-extern "C" {
-#endif  /* __cplusplus */
-
-epicsShareFunc char * epicsStrtok_r(char *s, const char *delim, char **lasts);
-
-#ifdef __cplusplus
-}
-#endif  /* __cplusplus */
