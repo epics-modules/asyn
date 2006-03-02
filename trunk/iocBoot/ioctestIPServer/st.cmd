5551b1e4f22e8133e18ed4c5559620f5ae760bb8
rivers <rivers@9d289d9b-4bc5-ceb4-995e-c1689a017bcc>
2006-03-02 18:33:16 +0000

    Added ipSNCServer sequence program

--- trunk/iocBoot/ioctestIPServer/st.cmd
+++ trunk/iocBoot/ioctestIPServer/st.cmd
@@ -5,14 +5,16 @@ testIPServer_registerRecordDeviceDriver(pdbbase)
 
 #The following command starts a server on port 5000
 drvAsynIPServerPortConfigure("P5000","localhost:5000",2,0,0,0)
+drvAsynIPServerPortConfigure("P5001","localhost:5001",1,0,0,0)
 
-asynSetTraceFile("P5000",-1,"")
-asynSetTraceMask("P5000",-1,0xff)
-asynSetTraceIOMask("P5000",-1,0x2)
+#asynSetTraceFile("P5000",-1,"")
+#asynSetTraceMask("P5000",-1,0xff)
+#asynSetTraceIOMask("P5000",-1,0x2)
 
-dbLoadRecords("../../db/asynRecord.db", "P=testIPServer:, R=asyn1, PORT=junk, ADDR=0, IMAX=80, OMAX=80")
+dbLoadRecords("../../db/testIPServer.db", "P=testIPServer:")
 
 iocInit()
 
-testIPServer("P5000")
+ipEchoServer("P5000")
+seq("ipSNCServer", "P=testIPServer:, PORT=P5001")
 
