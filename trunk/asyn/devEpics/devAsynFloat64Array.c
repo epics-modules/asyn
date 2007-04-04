dbde75506fa44b2f3685d6b1e07e7d97e2f34746
rivers <rivers@9d289d9b-4bc5-ceb4-995e-c1689a017bcc>
2007-04-04 19:06:20 +0000

    Added support for callbacks for I/O Intr scanning

--- trunk/asyn/devEpics/devAsynFloat64Array.c
+++ trunk/asyn/devEpics/devAsynFloat64Array.c
@@ -30,6 +30,7 @@
 #include <waveformRecord.h>
 #include <recSup.h>
 #include <devSup.h>
+#include <menuFtype.h>
 
 #include <epicsExport.h>
 #include "asynDriver.h"
@@ -43,21 +44,32 @@ typedef struct devAsynWfPvt{
     asynUser        *pasynUser;
     asynFloat64Array  *pfloat64Array;
     void            *float64ArrayPvt;
+    void            *registrarPvt;
     int             canBlock;
     CALLBACK        callback;
     IOSCANPVT       ioScanPvt;
+    asynStatus      status;
+    int             gotValue; /*For interruptCallbackInput */
+    int             nord;
+    interruptCallbackFloat64Array interruptCallback;
     char            *portName;
     char            *userParam;
     int             addr;
 }devAsynWfPvt;
 
-static long initCommon(dbCommon *pr, DBLINK *plink, userCallback callback);
+static long getIoIntInfo(int cmd, dbCommon *pr, IOSCANPVT *iopvt);
+static long initCommon(dbCommon *pr, DBLINK *plink,
+    userCallback callback, interruptCallbackFloat64Array interruptCallback);
 static long processCommon(dbCommon *pr);
 static long initWfFloat64Array(waveformRecord *pwf);
 static long initWfOutFloat64Array(waveformRecord *pwf);
 /* processCommon callbacks */
 static void callbackWf(asynUser *pasynUser);
 static void callbackWfOut(asynUser *pasynUser);
+static void interruptCallbackInput(void *drvPvt, asynUser *pasynUser, 
+                epicsFloat64 *value, size_t len);
+static void interruptCallbackOutput(void *drvPvt, asynUser *pasynUser, 
+                epicsFloat64 *value, size_t len);
 
 typedef struct analogDset { /* analog  dset */
     long        number;
@@ -70,15 +82,17 @@ typedef struct analogDset { /* analog  dset */
 } analogDset;
 
 analogDset asynFloat64ArrayWfIn =
-    {6, 0, 0, initWfFloat64Array,    0, processCommon, 0};
+    {6, 0, 0, initWfFloat64Array,    getIoIntInfo, processCommon, 0};
 analogDset asynFloat64ArrayWfOut =
-    {6, 0, 0, initWfOutFloat64Array, 0, processCommon, 0};
+    {6, 0, 0, initWfOutFloat64Array, getIoIntInfo, processCommon, 0};
 
 epicsExportAddress(dset, asynFloat64ArrayWfIn);
 epicsExportAddress(dset, asynFloat64ArrayWfOut);
 
-static long initCommon(dbCommon *pr, DBLINK *plink, userCallback callback)
+static long initCommon(dbCommon *pr, DBLINK *plink, 
+    userCallback callback, interruptCallbackFloat64Array interruptCallback)
 {
+    waveformRecord *pwf = (waveformRecord *)pr;
     devAsynWfPvt *pPvt;
     asynStatus status;
     asynUser *pasynUser;
@@ -90,6 +104,12 @@ static long initCommon(dbCommon *pr, DBLINK *plink, userCallback callback)
     pasynUser = pasynManager->createAsynUser(callback, 0);
     pasynUser->userPvt = pPvt;
     pPvt->pasynUser = pasynUser;
+    /* This device support only supports 32-bit float data type */
+    if (pwf->ftvl != menuFtypeFLOAT) {
+        errlogPrintf("devAsynWf::initCommon, %s field type must be FLOAT\n",
+                     pr->name);
+        goto bad;
+    }
     /* Parse the link to get addr and port */
     status = pasynEpicsUtils->parseLink(pasynUser, plink,
                 &pPvt->portName, &pPvt->addr, &pPvt->userParam);
@@ -129,6 +149,8 @@ static long initCommon(dbCommon *pr, DBLINK *plink, userCallback callback)
     }
     pPvt->pfloat64Array = pasynInterface->pinterface;
     pPvt->float64ArrayPvt = pasynInterface->drvPvt;
+    scanIoInit(&pPvt->ioScanPvt);
+    pPvt->interruptCallback = interruptCallback;
     /* Determine if device can block */
     pasynManager->canBlock(pasynUser, &pPvt->canBlock);
     return 0;
@@ -137,18 +159,59 @@ bad:
    return -1;
 }
 
+static long getIoIntInfo(int cmd, dbCommon *pr, IOSCANPVT *iopvt)
+{
+    devAsynWfPvt *pPvt = (devAsynWfPvt *)pr->dpvt;
+    asynStatus status;
+
+    /* If initCommon failed then pPvt->pfloat64Array is NULL, return error */
+    if (!pPvt->pfloat64Array) return -1;
+
+    if (cmd == 0) {
+        /* Add to scan list.  Register interrupts */
+        asynPrint(pPvt->pasynUser, ASYN_TRACE_FLOW,
+            "%s devAsynFloat64Array::getIoIntInfo registering interrupt\n",
+            pr->name);
+        status = pPvt->pfloat64Array->registerInterruptUser(
+           pPvt->float64ArrayPvt, pPvt->pasynUser,
+           pPvt->interruptCallback, pPvt, &pPvt->registrarPvt);
+        if(status!=asynSuccess) {
+            asynPrint(pPvt->pasynUser, ASYN_TRACE_ERROR,
+                      "%s devAsynFloat64Array registerInterruptUser %s\n",
+                      pr->name, pPvt->pasynUser->errorMessage);
+        }
+    } else {
+        asynPrint(pPvt->pasynUser, ASYN_TRACE_FLOW,
+            "%s devAsynFloat64Array::getIoIntInfo cancelling interrupt\n",
+             pr->name);
+        status = pPvt->pfloat64Array->cancelInterruptUser(pPvt->float64ArrayPvt,
+             pPvt->pasynUser, pPvt->registrarPvt);
+        if(status!=asynSuccess) {
+            asynPrint(pPvt->pasynUser, ASYN_TRACE_ERROR,
+                      "%s devAsynFloat64Array cancelInterruptUser %s\n",
+                      pr->name,pPvt->pasynUser->errorMessage);
+        }
+    }
+    *iopvt = pPvt->ioScanPvt;
+    return 0;
+}
+
+
 static long initWfOutFloat64Array(waveformRecord *pwf)
-{ return  initCommon((dbCommon *)pwf, (DBLINK *)&pwf->inp, callbackWfOut); } 
+{ return  initCommon((dbCommon *)pwf, (DBLINK *)&pwf->inp, 
+    callbackWfOut, interruptCallbackOutput); } 
 
 static long initWfFloat64Array(waveformRecord *pwf)
-{ return initCommon((dbCommon *)pwf, (DBLINK *)&pwf->inp, callbackWf); } 
+{ return initCommon((dbCommon *)pwf, (DBLINK *)&pwf->inp, 
+    callbackWf, interruptCallbackInput); } 
 
 static long processCommon(dbCommon *pr)
 {
     devAsynWfPvt *pPvt = (devAsynWfPvt *)pr->dpvt;
+    waveformRecord *pwf = (waveformRecord *)pr;
     int status;
 
-    if (pr->pact == 0) {   /* This is an initial call from record */
+    if (!pPvt->gotValue && !pr->pact) {   /* This is an initial call from record */
         if(pPvt->canBlock) pr->pact = 1;
         status = pasynManager->queueRequest(pPvt->pasynUser, 0, 0);
         if((status==asynSuccess) && pPvt->canBlock) return 0;
@@ -160,6 +223,8 @@ static long processCommon(dbCommon *pr)
             recGblSetSevr(pr, READ_ALARM, INVALID_ALARM);
         }
     }
+    if (pPvt->gotValue) pwf->nord = pPvt->nord;
+    pPvt->gotValue = 0;
     return 0;
 } 
 
@@ -210,5 +275,36 @@ static void callbackWf(asynUser *pasynUser)
         prset->process(pwf);
         dbScanUnlock((dbCommon *)pwf);
     }
+}
+
+static void interruptCallbackInput(void *drvPvt, asynUser *pasynUser, 
+                epicsFloat64 *value, size_t len)
+{
+    devAsynWfPvt *pPvt = (devAsynWfPvt *)drvPvt;
+    waveformRecord *pwf = (waveformRecord *)pPvt->pr;
+    int i;
+    epicsFloat64 *pfloat64 = (epicsFloat64 *)pwf->bptr;
+
+    asynPrintIO(pPvt->pasynUser, ASYN_TRACEIO_DEVICE,
+        (char *)value, len*sizeof(epicsFloat64),
+        "%s devAsynFloat64Array::interruptCallbackInput\n",
+        pwf->name);
+    if (len > pwf->nelm) len = pwf->nelm;
+    for (i=0; i<len; i++) pfloat64[i] = value[i];
+    pPvt->gotValue = 1;
+    pPvt->nord = len;
+    scanIoRequest(pPvt->ioScanPvt);
+}
+
+static void interruptCallbackOutput(void *drvPvt, asynUser *pasynUser, 
+                epicsFloat64 *value, size_t len)
+{
+    devAsynWfPvt *pPvt = (devAsynWfPvt *)drvPvt;
+    dbCommon *pr = pPvt->pr;
+
+    asynPrint(pPvt->pasynUser, ASYN_TRACE_ERROR,
+        "%s devAsynFloat64Array::interruptCallbackOutput not supported yet!\n",
+        pr->name);
+    /* scanOnce(pr); */
 }
 
