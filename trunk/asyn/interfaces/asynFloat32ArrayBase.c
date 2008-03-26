52fa57e2c4a6dd4b16764d3ce979843f7c232470
rivers <rivers@9d289d9b-4bc5-ceb4-995e-c1689a017bcc>
2008-03-26 17:17:31 +0000

    Rewrote to use asynXXXArrayBase.h, eliminating much redundant code

--- trunk/asyn/interfaces/asynFloat32ArrayBase.c
+++ trunk/asyn/interfaces/asynFloat32ArrayBase.c
@@ -9,6 +9,7 @@
 ***********************************************************************/
 
 /*  11-OCT-2004 Marty Kraimer
+ *  26-MAR-2008 Mark Rivers, converted to use macro
 */
 
 #include <epicsTypes.h>
@@ -19,125 +20,8 @@
 #include "asynDriver.h"
 #include "asynFloat32Array.h"
 
-static asynStatus initialize(const char *portName, asynInterface *pfloat32ArrayInterface);
+#include "asynXXXArrayBase.h"
+ 
+ASYN_XXX_ARRAY_BASE_FUNCS(asynFloat32Array, asynFloat32ArrayType, asynFloat32ArrayBase, pasynFloat32ArrayBase,
+                          asynFloat32ArrayInterrupt, interruptCallbackFloat32Array, epicsFloat32);
 
-static asynFloat32ArrayBase float32ArrayBase = {initialize};
-epicsShareDef asynFloat32ArrayBase *pasynFloat32ArrayBase = &float32ArrayBase;
-
-static asynStatus writeDefault(void *drvPvt, asynUser *pasynUser,
-                               epicsFloat32 *value, size_t nelem);
-static asynStatus readDefault(void *drvPvt, asynUser *pasynUser,
-                              epicsFloat32 *value, size_t nelem, size_t *nIn);
-static asynStatus registerInterruptUser(void *drvPvt,asynUser *pasynUser,
-                               interruptCallbackFloat32Array callback, void *userPvt,
-                               void **registrarPvt);
-static asynStatus cancelInterruptUser(void *drvPvt, asynUser *pasynUser,
-                               void *registrarPvt);
-
-
-asynStatus initialize(const char *portName, asynInterface *pdriver)
-{
-    asynFloat32Array *pasynFloat32Array =
-                      (asynFloat32Array *)pdriver->pinterface;
-
-    if(!pasynFloat32Array->write) pasynFloat32Array->write = writeDefault;
-    if(!pasynFloat32Array->read) pasynFloat32Array->read = readDefault;
-    if(!pasynFloat32Array->registerInterruptUser)
-        pasynFloat32Array->registerInterruptUser = registerInterruptUser;
-    if(!pasynFloat32Array->cancelInterruptUser)
-        pasynFloat32Array->cancelInterruptUser = cancelInterruptUser;
-    return pasynManager->registerInterface(portName,pdriver);
-}
-
-static asynStatus writeDefault(void *drvPvt, asynUser *pasynUser,
-    epicsFloat32 *value, size_t nelem)
-{
-    const char *portName;
-    asynStatus status;
-    int        addr;
-    
-    status = pasynManager->getPortName(pasynUser,&portName);
-    if(status!=asynSuccess) return status;
-    status = pasynManager->getAddr(pasynUser,&addr);
-    if(status!=asynSuccess) return status;
-    epicsSnprintf(pasynUser->errorMessage,pasynUser->errorMessageSize,
-        "write is not supported\n");
-    asynPrint(pasynUser,ASYN_TRACE_ERROR,
-        "%s %d write is not supported\n",portName,addr);
-    return asynError;
-}
-
-static asynStatus readDefault(void *drvPvt, asynUser *pasynUser,
-    epicsFloat32 *value, size_t nelem, size_t *nIn)
-{
-    const char *portName;
-    asynStatus status;
-    int        addr;
-    
-    status = pasynManager->getPortName(pasynUser,&portName);
-    if(status!=asynSuccess) return status;
-    status = pasynManager->getAddr(pasynUser,&addr);
-    if(status!=asynSuccess) return status;
-    epicsSnprintf(pasynUser->errorMessage,pasynUser->errorMessageSize,
-        "write is not supported\n");
-    asynPrint(pasynUser,ASYN_TRACE_ERROR,
-        "%s %d read is not supported\n",portName,addr);
-    return asynError;
-}
-
-
-static asynStatus registerInterruptUser(void *drvPvt,asynUser *pasynUser,
-                               interruptCallbackFloat32Array callback, void *userPvt,
-                               void **registrarPvt)
-{
-    const char    *portName;
-    asynStatus    status;
-    int           addr;
-    interruptNode *pinterruptNode;
-    void          *pinterruptPvt;
-    asynFloat32ArrayInterrupt *pasynFloat32ArrayInterrupt;
-
-    status = pasynManager->getPortName(pasynUser,&portName);
-    if(status!=asynSuccess) return status;
-    status = pasynManager->getAddr(pasynUser,&addr);
-    if(status!=asynSuccess) return status;
-    status = pasynManager->getInterruptPvt(pasynUser, asynFloat32ArrayType,
-                                           &pinterruptPvt);
-    if(status!=asynSuccess) return status;
-    pinterruptNode = pasynManager->createInterruptNode(pinterruptPvt);
-    if(status!=asynSuccess) return status;
-    pasynFloat32ArrayInterrupt = pasynManager->memMalloc(
-                                             sizeof(asynFloat32ArrayInterrupt));
-    pinterruptNode->drvPvt = pasynFloat32ArrayInterrupt;
-    pasynFloat32ArrayInterrupt->pasynUser =
-                        pasynManager->duplicateAsynUser(pasynUser, NULL, NULL);
-    pasynFloat32ArrayInterrupt->addr = addr;
-    pasynFloat32ArrayInterrupt->callback = callback;
-    pasynFloat32ArrayInterrupt->userPvt = userPvt;
-    *registrarPvt = pinterruptNode;
-    asynPrint(pasynUser,ASYN_TRACE_FLOW,
-        "%s %d registerInterruptUser\n",portName,addr);
-    return pasynManager->addInterruptUser(pasynUser,pinterruptNode);
-}
-
-static asynStatus cancelInterruptUser(void *drvPvt, asynUser *pasynUser,
-    void *registrarPvt)
-{
-    interruptNode *pinterruptNode = (interruptNode *)registrarPvt;
-    asynStatus    status;
-    const char    *portName;
-    int           addr;
-    asynFloat32ArrayInterrupt *pasynFloat32ArrayInterrupt = 
-        (asynFloat32ArrayInterrupt *)pinterruptNode->drvPvt;
-    
-    status = pasynManager->getPortName(pasynUser,&portName);
-    if(status!=asynSuccess) return status;
-    status = pasynManager->getAddr(pasynUser,&addr);
-    if(status!=asynSuccess) return status;
-    asynPrint(pasynUser,ASYN_TRACE_FLOW,
-        "%s %d cancelInterruptUser\n",portName,addr);
-    status = pasynManager->removeInterruptUser(pasynUser,pinterruptNode);
-    pasynManager->freeAsynUser(pasynFloat32ArrayInterrupt->pasynUser);
-    pasynManager->memFree(pasynFloat32ArrayInterrupt, sizeof(asynFloat32ArrayInterrupt));
-    return status;
-}
--- trunk/asyn/interfaces/asynInt8ArrayBase.c
+++ trunk/asyn/interfaces/asynInt8ArrayBase.c
@@ -9,6 +9,7 @@
 ***********************************************************************/
 
 /*  11-OCT-2004 Marty Kraimer
+ *  26-MAR-2008 Mark Rivers, converted to use macro
 */
 
 #include <epicsTypes.h>
@@ -19,122 +20,8 @@
 #include "asynDriver.h"
 #include "asynInt8Array.h"
 
-static asynStatus initialize(const char *portName, asynInterface *pint8ArrayInterface);
+#include "asynXXXArrayBase.h"
+ 
+ASYN_XXX_ARRAY_BASE_FUNCS(asynInt8Array, asynInt8ArrayType, asynInt8ArrayBase, pasynInt8ArrayBase,
+                          asynInt8ArrayInterrupt, interruptCallbackInt8Array, epicsInt8);
 
-static asynInt8ArrayBase int8ArrayBase = {initialize};
-epicsShareDef asynInt8ArrayBase *pasynInt8ArrayBase = &int8ArrayBase;
-
-static asynStatus writeDefault(void *drvPvt, asynUser *pasynUser,
-                               epicsInt8 *value, size_t nelem);
-static asynStatus readDefault(void *drvPvt, asynUser *pasynUser,
-                              epicsInt8 *value, size_t nelem, size_t *nIn);
-static asynStatus registerInterruptUser(void *drvPvt,asynUser *pasynUser,
-                               interruptCallbackInt8Array callback, void *userPvt,
-                               void **registrarPvt);
-static asynStatus cancelInterruptUser(void *drvPvt, asynUser *pasynUser,
-                               void *registrarPvt);
-
-
-asynStatus initialize(const char *portName, asynInterface *pdriver)
-{
-    asynInt8Array *pasynInt8Array = (asynInt8Array *)pdriver->pinterface;
-
-    if(!pasynInt8Array->write) pasynInt8Array->write = writeDefault;
-    if(!pasynInt8Array->read) pasynInt8Array->read = readDefault;
-    if(!pasynInt8Array->registerInterruptUser)
-        pasynInt8Array->registerInterruptUser = registerInterruptUser;
-    if(!pasynInt8Array->cancelInterruptUser)
-        pasynInt8Array->cancelInterruptUser = cancelInterruptUser;
-    return pasynManager->registerInterface(portName,pdriver);
-}
-
-static asynStatus writeDefault(void *drvPvt, asynUser *pasynUser,
-    epicsInt8 *value, size_t nelem)
-{
-    const char *portName;
-    asynStatus status;
-    int        addr;
-    
-    status = pasynManager->getPortName(pasynUser,&portName);
-    if(status!=asynSuccess) return status;
-    status = pasynManager->getAddr(pasynUser,&addr);
-    if(status!=asynSuccess) return status;
-    epicsSnprintf(pasynUser->errorMessage,pasynUser->errorMessageSize,
-        "write is not supported\n");
-    asynPrint(pasynUser,ASYN_TRACE_ERROR,
-        "%s %d write is not supported\n",portName,addr);
-    return asynError;
-}
-
-static asynStatus readDefault(void *drvPvt, asynUser *pasynUser,
-    epicsInt8 *value, size_t nelem, size_t *nIn)
-{
-    const char *portName;
-    asynStatus status;
-    int        addr;
-    
-    status = pasynManager->getPortName(pasynUser,&portName);
-    if(status!=asynSuccess) return status;
-    status = pasynManager->getAddr(pasynUser,&addr);
-    if(status!=asynSuccess) return status;
-    epicsSnprintf(pasynUser->errorMessage,pasynUser->errorMessageSize,
-        "write is not supported\n");
-    asynPrint(pasynUser,ASYN_TRACE_ERROR,
-        "%s %d read is not supported\n",portName,addr);
-    return asynError;
-}
-
-
-static asynStatus registerInterruptUser(void *drvPvt,asynUser *pasynUser,
-      interruptCallbackInt8Array callback, void *userPvt,void **registrarPvt)
-{
-    const char    *portName;
-    asynStatus    status;
-    int           addr;
-    interruptNode *pinterruptNode;
-    void          *pinterruptPvt;
-    asynInt8ArrayInterrupt *pasynInt8ArrayInterrupt;
-
-    status = pasynManager->getPortName(pasynUser,&portName);
-    if(status!=asynSuccess) return status;
-    status = pasynManager->getAddr(pasynUser,&addr);
-    if(status!=asynSuccess) return status;
-    status = pasynManager->getInterruptPvt(pasynUser, asynInt8ArrayType,
-                                           &pinterruptPvt);
-    if(status!=asynSuccess) return status;
-    pinterruptNode = pasynManager->createInterruptNode(pinterruptPvt);
-    if(status!=asynSuccess) return status;
-    pasynInt8ArrayInterrupt = pasynManager->memMalloc(
-                                             sizeof(asynInt8ArrayInterrupt));
-    pinterruptNode->drvPvt = pasynInt8ArrayInterrupt;
-    pasynInt8ArrayInterrupt->pasynUser =
-                       pasynManager->duplicateAsynUser(pasynUser, NULL, NULL);
-    pasynInt8ArrayInterrupt->addr = addr;
-    pasynInt8ArrayInterrupt->callback = callback;
-    pasynInt8ArrayInterrupt->userPvt = userPvt;
-    *registrarPvt = pinterruptNode;
-    asynPrint(pasynUser,ASYN_TRACE_FLOW,
-        "%s %d registerInterruptUser\n",portName,addr);
-    return pasynManager->addInterruptUser(pasynUser,pinterruptNode);
-}
-
-static asynStatus cancelInterruptUser(void *drvPvt, asynUser *pasynUser,void *registrarPvt)
-{
-    interruptNode *pinterruptNode = (interruptNode *)registrarPvt;
-    asynStatus    status;
-    const char    *portName;
-    int           addr;
-    asynInt8ArrayInterrupt *pasynInt8ArrayInterrupt =
-                  (asynInt8ArrayInterrupt *)pinterruptNode->drvPvt;
-    
-    status = pasynManager->getPortName(pasynUser,&portName);
-    if(status!=asynSuccess) return status;
-    status = pasynManager->getAddr(pasynUser,&addr);
-    if(status!=asynSuccess) return status;
-    asynPrint(pasynUser,ASYN_TRACE_FLOW,
-        "%s %d cancelInterruptUser\n",portName,addr);
-    status = pasynManager->removeInterruptUser(pasynUser,pinterruptNode);
-    pasynManager->freeAsynUser(pasynInt8ArrayInterrupt->pasynUser);
-    pasynManager->memFree(pasynInt8ArrayInterrupt, sizeof(asynInt8ArrayInterrupt));
-    return status;
-}
