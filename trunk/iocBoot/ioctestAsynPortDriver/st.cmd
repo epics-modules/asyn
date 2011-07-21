5db07b603b2805be76a3ff57f9f3dc6403a1a651
rivers <rivers@9d289d9b-4bc5-ceb4-995e-c1689a017bcc>
2011-07-21 18:22:06 +0000

    Load asyn record

--- trunk/iocBoot/ioctestAsynPortDriver/st.cmd
+++ trunk/iocBoot/ioctestAsynPortDriver/st.cmd
@@ -7,6 +7,7 @@ testAsynPortDriver_registerRecordDeviceDriver(pdbbase)
 testAsynPortDriverConfigure("testAPD", 1000)
 
 dbLoadRecords("../../db/testAsynPortDriver.db","P=testAPD:,R=scope1:,PORT=testAPD,ADDR=0,TIMEOUT=1,NPOINTS=1000")
+dbLoadRecords("../../db/asynRecord.db","P=testAPD:,R=asyn1,PORT=testAPD,ADDR=0,OMAX=80,IMAX=80")
 #asynSetTraceMask("testAPD",0,0xff)
 asynSetTraceIOMask("testAPD",0,0x2)
 iocInit()
