dccd4138a15e0cc73fe4becc58acadf5aa5b96e6
rivers <rivers@9d289d9b-4bc5-ceb4-995e-c1689a017bcc>
2012-04-20 14:28:04 +0000

    Initialize commConfigSize, needed in recent Windows

--- trunk/asyn/drvAsynSerial/drvAsynSerialPortWin32.c
+++ trunk/asyn/drvAsynSerial/drvAsynSerialPortWin32.c
@@ -86,7 +86,7 @@ getOption(void *drvPvt, asynUser *pasynUser,
                               const char *key, char *val, int valSize)
 {
     ttyController_t *tty = (ttyController_t *)drvPvt;
-    DWORD commConfigSize;
+    DWORD commConfigSize = sizeof(tty->commConfig);
     BOOL ret;
     DWORD error;
     int l;
@@ -162,7 +162,7 @@ static asynStatus
 setOption(void *drvPvt, asynUser *pasynUser, const char *key, const char *val)
 {
     ttyController_t *tty = (ttyController_t *)drvPvt;
-    DWORD commConfigSize;
+    DWORD commConfigSize = sizeof(tty->commConfig);
     BOOL ret;
     DWORD error;
 
