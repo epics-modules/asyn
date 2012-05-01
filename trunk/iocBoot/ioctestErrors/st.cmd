1a934d896106539f51861045c1e910f5d43d5d45
rivers <rivers@9d289d9b-4bc5-ceb4-995e-c1689a017bcc>
2012-05-01 22:59:39 +0000

    New directory for testErrorsApp

---
+++ trunk/iocBoot/ioctestErrors/st.cmd
@@ -0,0 +1,10 @@
+dbLoadDatabase("../../dbd/testErrors.dbd")
+testErrors_registerRecordDeviceDriver(pdbbase)
+
+testErrorsConfigure("PORT1",)
+
+dbLoadRecords("../../db/testErrors.db","P=testErrors:,R=test1:,PORT=PORT1,ADDR=0,TIMEOUT=1")
+dbLoadRecords("../../db/asynRecord.db","P=testErrors:,R=asyn1,PORT=PORT1,ADDR=0,OMAX=80,IMAX=80")
+#asynSetTraceMask("PORT1",0,0xff)
+asynSetTraceIOMask("PORT1",0,0x2)
+iocInit()
