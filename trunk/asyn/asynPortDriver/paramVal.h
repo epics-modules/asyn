74826d2a9ccae616bc02e970fa12f44db6cc3bad
rivers <rivers@9d289d9b-4bc5-ceb4-995e-c1689a017bcc>
2012-05-01 22:48:31 +0000

    Added setStatus() and getStatus(); added functions like getInteger() to get the data values; changed ParamVal to paramVal for consistency

--- trunk/asyn/asynPortDriver/paramVal.h
+++ trunk/asyn/asynPortDriver/paramVal.h
@@ -1,33 +1,47 @@
-#ifndef asynParamVal_H
-#define asynParamVal_H
+#ifndef asynparamVal_H
+#define asynparamVal_H
 
 #include "stdio.h"
 #include "epicsTypes.h"
 #include "asynParamType.h"
+#include "asynDriver.h"
 #ifdef __cplusplus
 
 /** Structure for storing parameter value in parameter library */
-class epicsShareFunc ParamVal {
+class epicsShareFunc paramVal {
 public:
-	ParamVal(const char *name);
-	ParamVal(const char *name, asynParamType type);
-	bool isDefined();
-	void setDefined(bool defined);
-	bool hasValueChanged();
-	void setValueChanged();
-	void resetValueChanged();
-	char* getName();
-	bool nameEquals(const char* name);
-	void setInteger(int value);
-	void setUInt32(epicsUInt32 value, epicsUInt32 valueMask, epicsUInt32 interruptMask);
-	void setDouble(double value);
-	void setString(const char *value);
-	void report(int id, FILE *fp, int details);
-	const char* getTypeName();
-	asynParamType type; /**< Parameter data type */
+    paramVal(const char *name);
+    paramVal(const char *name, asynParamType type);
+    bool isDefined();
+    void setDefined(bool defined);
+    bool hasValueChanged();
+    void setValueChanged();
+    void resetValueChanged();
+    void setStatus(asynStatus status);
+    asynStatus getStatus();
+    char* getName();
+    bool nameEquals(const char* name);
+    void setInteger(epicsInt32 value);
+    epicsInt32 getInteger();
+    void setUInt32(epicsUInt32 value, epicsUInt32 valueMask, epicsUInt32 interruptMask);
+    epicsUInt32 getUInt32(epicsUInt32 valueMask);
+    void setDouble(epicsFloat64 value);
+    epicsFloat64 getDouble();
+    void setString(const char *value);
+    char *getString();
+    void report(int id, FILE *fp, int details);
+    const char* getTypeName();
+    asynParamType type; /**< Parameter data type */
+    static const char* typeNames[];
     epicsUInt32 uInt32RisingMask;
     epicsUInt32 uInt32FallingMask;
     epicsUInt32 uInt32CallbackMask;
+
+protected:
+    asynStatus status_;
+    bool valueDefined;
+    bool valueChanged;
+    char *name;         /**< Parameter name */
     /** Union for parameter value */
     union
     {
@@ -35,19 +49,13 @@ public:
         epicsUInt32  uival;
         epicsFloat64 dval;
         char         *sval;
-        epicsInt8     *pi8;
-        epicsInt16    *pi16;
-        epicsInt32    *pi32;
-        epicsFloat32  *pf32;
-        epicsFloat64  *pf64;
+        epicsInt8    *pi8;
+        epicsInt16   *pi16;
+        epicsInt32   *pi32;
+        epicsFloat32 *pf32;
+        epicsFloat64 *pf64;
         void         *pgp;
     } data;
-    static const char* typeNames[];
-
-protected:
-    bool valueDefined;
-    bool valueChanged;
-    char *name;         /**< Parameter name */
 };
 
 #endif /* cplusplus */
