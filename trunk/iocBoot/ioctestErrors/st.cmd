410e0fce098c88ec03f607af21cec832ebd45a34
rivers <rivers@9d289d9b-4bc5-ceb4-995e-c1689a017bcc>
2012-05-02 23:34:08 +0000

    Added SCAN macro to allow easily changing input records from I/O Intr scan to periodic scan

--- trunk/iocBoot/ioctestErrors/st.cmd
+++ trunk/iocBoot/ioctestErrors/st.cmd
@@ -3,7 +3,7 @@ testErrors_registerRecordDeviceDriver(pdbbase)
 
 testErrorsConfigure("PORT1",)
 
-dbLoadRecords("../../db/testErrors.db","P=testErrors:,R=test1:,PORT=PORT1,ADDR=0,TIMEOUT=1")
+dbLoadRecords("../../db/testErrors.db","P=testErrors:,R=test1:,PORT=PORT1,ADDR=0,TIMEOUT=1,SCAN=1 second")
 dbLoadRecords("../../db/asynRecord.db","P=testErrors:,R=asyn1,PORT=PORT1,ADDR=0,OMAX=80,IMAX=80")
 #asynSetTraceMask("PORT1",0,0xff)
 asynSetTraceIOMask("PORT1",0,0x2)
