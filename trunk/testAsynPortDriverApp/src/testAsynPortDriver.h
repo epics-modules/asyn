68eeaa18e7c575cf4e795487704006b13d311cd7
rivers <rivers@9d289d9b-4bc5-ceb4-995e-c1689a017bcc>
2012-05-06 21:47:33 +0000

    Changes to support testing the asynEnum interface

--- trunk/testAsynPortDriverApp/src/testAsynPortDriver.h
+++ trunk/testAsynPortDriverApp/src/testAsynPortDriver.h
@@ -14,21 +14,28 @@
 
 #include "asynPortDriver.h"
 
+#define NUM_VERT_SELECTIONS 4
+
+
 /* These are the drvInfo strings that are used to identify the parameters.
  * They are used by asyn clients, including standard asyn device support */
-#define P_RunString                "SCOPE_RUN"             /* asynInt32,    r/w */
-#define P_MaxPointsString          "SCOPE_MAX_POINTS"      /* asynInt32,    r/o */
-#define P_TimePerDivisionString    "SCOPE_TIME_PER_DIV"    /* asynFloat64,  r/w */
-#define P_VoltsPerDivisionString   "SCOPE_VOLTS_PER_DIV"   /* asynFloat64,  r/w */
-#define P_VoltOffsetString         "SCOPE_VOLT_OFFSET"     /* asynFloat64,  r/w */
-#define P_TriggerDelayString       "SCOPE_TRIGGER_DELAY"   /* asynFloat64,  r/w */
-#define P_NoiseAmplitudeString     "SCOPE_NOISE_AMPLITUDE" /* asynFloat64,  r/w */
-#define P_UpdateTimeString         "SCOPE_UPDATE_TIME"     /* asynFloat64,  r/w */
-#define P_WaveformString           "SCOPE_WAVEFORM"        /* asynFloat64Array,  r/o */
-#define P_TimeBaseString           "SCOPE_TIME_BASE"       /* asynFloat64Array,  r/o */
-#define P_MinValueString           "SCOPE_MIN_VALUE"       /* asynFloat64,  r/o */
-#define P_MaxValueString           "SCOPE_MAX_VALUE"       /* asynFloat64,  r/o */
-#define P_MeanValueString          "SCOPE_MEAN_VALUE"      /* asynFloat64,  r/o */
+#define P_RunString                "SCOPE_RUN"                  /* asynInt32,    r/w */
+#define P_MaxPointsString          "SCOPE_MAX_POINTS"           /* asynInt32,    r/o */
+#define P_TimePerDivString         "SCOPE_TIME_PER_DIV"         /* asynFloat64,  r/w */
+#define P_TimePerDivSelectString   "SCOPE_TIME_PER_DIV_SELECT"  /* asynInt32,    r/w */
+#define P_VertGainString           "SCOPE_VERT_GAIN"            /* asynFloat64,  r/w */
+#define P_VertGainSelectString     "SCOPE_VERT_GAIN_SELECT"     /* asynInt32,    r/w */
+#define P_VoltsPerDivString        "SCOPE_VOLTS_PER_DIV"        /* asynFloat64,  r/w */
+#define P_VoltsPerDivSelectString  "SCOPE_VOLTS_PER_DIV_SELECT" /* asynInt32,    r/w */
+#define P_VoltOffsetString         "SCOPE_VOLT_OFFSET"          /* asynFloat64,  r/w */
+#define P_TriggerDelayString       "SCOPE_TRIGGER_DELAY"        /* asynFloat64,  r/w */
+#define P_NoiseAmplitudeString     "SCOPE_NOISE_AMPLITUDE"      /* asynFloat64,  r/w */
+#define P_UpdateTimeString         "SCOPE_UPDATE_TIME"          /* asynFloat64,  r/w */
+#define P_WaveformString           "SCOPE_WAVEFORM"             /* asynFloat64Array,  r/o */
+#define P_TimeBaseString           "SCOPE_TIME_BASE"            /* asynFloat64Array,  r/o */
+#define P_MinValueString           "SCOPE_MIN_VALUE"            /* asynFloat64,  r/o */
+#define P_MaxValueString           "SCOPE_MAX_VALUE"            /* asynFloat64,  r/o */
+#define P_MeanValueString          "SCOPE_MEAN_VALUE"           /* asynFloat64,  r/o */
 
 /** Class that demonstrates the use of the asynPortDriver base class to greatly simplify the task
   * of writing an asyn port driver.
@@ -45,6 +52,8 @@ public:
     virtual asynStatus writeFloat64(asynUser *pasynUser, epicsFloat64 value);
     virtual asynStatus readFloat64Array(asynUser *pasynUser, epicsFloat64 *value,
                                         size_t nElements, size_t *nIn);
+    virtual asynStatus readEnum(asynUser *pasynUser, char *strings[], int values[], int severities[],
+                                size_t nElements, size_t *nIn);
 
     /* These are the methods that are new to this class */
     void simTask(void);
@@ -54,8 +63,12 @@ protected:
     int P_Run;
     #define FIRST_SCOPE_COMMAND P_Run
     int P_MaxPoints;
-    int P_TimePerDivision;
-    int P_VoltsPerDivision;
+    int P_TimePerDiv;
+    int P_TimePerDivSelect;
+    int P_VertGain;
+    int P_VertGainSelect;
+    int P_VoltsPerDiv;
+    int P_VoltsPerDivSelect;
     int P_VoltOffset;
     int P_TriggerDelay;
     int P_NoiseAmplitude;
@@ -69,9 +82,16 @@ protected:
  
 private:
     /* Our data */
-    epicsEventId eventId;
-    epicsFloat64 *pData;
-    epicsFloat64 *pTimeBase;
+    epicsEventId eventId_;
+    epicsFloat64 *pData_;
+    epicsFloat64 *pTimeBase_;
+    // Actual volts per division are these values divided by vertical gain
+    char *voltsPerDivStrings_[NUM_VERT_SELECTIONS];
+    int voltsPerDivValues_[NUM_VERT_SELECTIONS];
+    int voltsPerDivSeverities_[NUM_VERT_SELECTIONS];
+    void setVertGain();
+    void setVoltsPerDiv();
+    void setTimePerDiv();
 };
 
 
