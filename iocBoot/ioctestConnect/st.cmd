0f38696e92df80ad18abd636838ffd2a3a0aa0c5
Mark Rivers <MarkRivers@users.noreply.github.com>
2012-07-26 16:08:55 +0000

    New controller IP address

--- iocBoot/ioctestConnect/st.cmd
+++ iocBoot/ioctestConnect/st.cmd
@@ -4,7 +4,7 @@ testConnect_registerRecordDeviceDriver(pdbbase)
 # This is a bogus IP address that will never connect
 #drvAsynIPPortConfigure("IPPort", "164.54.160.220:20", 0, 0, 1);
 # This is a real IP address that will connect
-drvAsynIPPortConfigure("IPPort", "newport-xps5:5001", 0, 0, 1);
+drvAsynIPPortConfigure("IPPort", "newport-xps9:5001", 0, 0, 1);
 testConnectConfigure("PORT1", "IPPort", "FirmwareVersionGet(char *)")
 asynSetTraceMask("PORT1",0,9)
 asynSetTraceIOMask("PORT1",0,0x2)
