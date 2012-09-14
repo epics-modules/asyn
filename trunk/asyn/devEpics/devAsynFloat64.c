edc6437113c3a66011af4835281f4e8f4bdeca40
rivers <rivers@9d289d9b-4bc5-ceb4-995e-c1689a017bcc>
2012-09-14 16:58:26 +0000

    Change ring buffer overflows from ASYN_TRACE_ERROR to ASYN_TRACEIO_DEVICE, because they are not really errors

--- trunk/asyn/devEpics/devAsynFloat64.c
+++ trunk/asyn/devEpics/devAsynFloat64.c
@@ -384,7 +384,7 @@ getCallbackValue(devPvt *pPvt)
     epicsMutexLock(pPvt->mutexId);
     if (pPvt->ringTail != pPvt->ringHead) {
         if (pPvt->ringBufferOverflows > 0) {
-            asynPrint(pPvt->pasynUser, ASYN_TRACE_ERROR,
+            asynPrint(pPvt->pasynUser, ASYN_TRACEIO_DEVICE,
                 "%s devAsynFloat64 getCallbackValue error, %d ring buffer overflows\n",
                                     pPvt->pr->name, pPvt->ringBufferOverflows);
             pPvt->ringBufferOverflows = 0;
