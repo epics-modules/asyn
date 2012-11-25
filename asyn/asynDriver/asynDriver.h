7d07e4e9ba9bc490fa8e587e4f9db7c5e4b4da2f
Mark Rivers <MarkRivers@users.noreply.github.com>
2012-11-25 21:37:16 +0000

    Changed ASYN_REVISION from 20 to 21

--- asyn/asynDriver/asynDriver.h
+++ asyn/asynDriver/asynDriver.h
@@ -23,7 +23,7 @@
 /* Version number names similar to those provide by base
  * These macros are always numeric */
 #define ASYN_VERSION       4
-#define ASYN_REVISION     20
+#define ASYN_REVISION     21
 #define ASYN_MODIFICATION  0
 
 #ifdef __cplusplus
