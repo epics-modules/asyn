c2e8323c417f92e6e4bdeae1eb15828622686239
Mark Rivers <MarkRivers@users.noreply.github.com>
2012-12-06 00:31:22 +0000

    Added asynSetOption to test that

--- iocBoot/ioctestErrors/st.cmd
+++ iocBoot/ioctestErrors/st.cmd
@@ -8,5 +8,9 @@ asynSetTraceIOMask("PORT1",0,0x2)
 dbLoadRecords("../../db/testErrors.db","P=testErrors:,R=test1:,PORT=PORT1,ADDR=0,TIMEOUT=1,SCAN=I/O Intr")
 #dbLoadRecords("../../db/testErrors.db","P=testErrors:,R=test1:,PORT=PORT1,ADDR=0,TIMEOUT=1,SCAN=2 second")
 dbLoadRecords("../../db/asynRecord.db","P=testErrors:,R=asyn1,PORT=PORT1,ADDR=0,OMAX=80,IMAX=80")
+
+asynSetOption("PORT1",0,"someKey","someValue")
+
 iocInit()
+
 
