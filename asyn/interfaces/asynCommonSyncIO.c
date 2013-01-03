c86daeef9b8fd2fcaf09e075738906e97671026e
Mark Rivers <MarkRivers@users.noreply.github.com>
2013-01-03 21:45:30 +0000

    Fixed bug that could cause crash if pasynCommonSyncIO->connect() failed

--- asyn/interfaces/asynCommonSyncIO.c
+++ asyn/interfaces/asynCommonSyncIO.c
@@ -67,6 +67,7 @@ static asynStatus connect(const char *port, int addr,
     asynInterface *pasynInterface;
 
     pioPvt = (ioPvt *)callocMustSucceed(1, sizeof(ioPvt),"asynCommonSyncIO");
+    pioPvt->connectEvent = epicsEventMustCreate(epicsEventEmpty);
     pasynUser = pasynManager->createAsynUser(connectDeviceCallback,0);
     pasynUser->userPvt = pioPvt;
     *ppasynUser = pasynUser;
@@ -99,7 +100,6 @@ static asynStatus connect(const char *port, int addr,
             }
         }
     }
-    pioPvt->connectEvent = epicsEventMustCreate(epicsEventEmpty);
     return asynSuccess ;
 }
 
