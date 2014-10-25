e2181e109bd4b0fabc2461445c97a30fe522a44a
Mark Rivers <MarkRivers@users.noreply.github.com>
2014-10-25 09:36:35 +0000

    Changed trace

--- iocBoot/ioctestArrayRingBuffer/st.cmd
+++ iocBoot/ioctestArrayRingBuffer/st.cmd
@@ -8,8 +8,9 @@ testArrayRingBufferConfigure("testARB", 100)
 
 dbLoadRecords("../../db/testArrayRingBuffer.db","P=testARB:,R=A1:,PORT=testARB,ADDR=0,TIMEOUT=1,NELM=100,RING_SIZE=10")
 dbLoadRecords("../../db/asynRecord.db","P=testARB:,R=asyn1,PORT=testARB,ADDR=0,OMAX=80,IMAX=80")
-#asynSetTraceMask("testARB",0,0xff)
-asynSetTraceIOMask("testARB",0,0x2)
+#asynSetTraceMask("testARB",0,0x21)
+#asynSetTraceMask("testARB",0,0xFF)
+asynSetTraceIOMask("testARB",0,0x4)
 iocInit()
 
 dbpf testARB:A1:Run 1
