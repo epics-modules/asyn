d9d49f1b98b3438a0467270b5cf68d875e5c6c49
rivers <rivers@9d289d9b-4bc5-ceb4-995e-c1689a017bcc>
2014-10-30 23:18:26 +0000

    Continued development

--- trunk/asyn/asynPortClient/asynPortClient.cpp
+++ trunk/asyn/asynPortClient/asynPortClient.cpp
@@ -11,14 +11,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdexcept>
 
 #define epicsExportSharedSymbols
 #include <shareLib.h>
 #include "asynPortClient.h"
 
-static const char *driverName = "asynPortClient";
-
-#define DEFAULT_TIMEOUT 1.0
+//static const char *driverName = "asynPortClient";
 
 asynClient::asynClient(const char *portName, int addr, const char *asynInterfaceType, const char *drvInfo, 
                        double timeout)
@@ -28,7 +27,13 @@ asynClient::asynClient(const char *portName, int addr, const char *asynInterface
     asynStatus status;
 
     status = pasynManager->connectDevice(pasynUser_, portName, addr);
-    pasynInterface_ = pasynManager->findInterface(pasynUser_, asynInterfaceType, 1);    
+    if (status) {
+        throw std::runtime_error(std::string("connectDevice failed:") + pasynUser_->errorMessage);
+    }
+    pasynInterface_ = pasynManager->findInterface(pasynUser_, asynInterfaceType, 1);
+    if (status) {
+        throw std::runtime_error(std::string("findInterface failed:") + asynInterfaceType);
+    }
 }
 
 asynClient::~asynClient()
@@ -47,16 +52,5 @@ void asynClient::report(FILE *fp, int details)
     fprintf(fp, "asynInterfaceType=%s\n", asynInterfaceType_);
     fprintf(fp, "drvInfo=%s\n", drvInfo_);
     fprintf(fp, "pasynUser=%p\n", pasynUser_);
-}
-
-//////////////////////////////////////////////////////////////////////
-///  asynInt32 class                                                //
-//////////////////////////////////////////////////////////////////////
-asynInt32Client::asynInt32Client(const char *portName, int addr, const char *drvInfo, double timeout=DEFAULT_TIMEOUT)
-    : asynClient(portName, addr, asynInt32SyncIOType, drvInfo, timeout)
-{
-    asynStatus status;
-    pasynInt32_ = (asynInt32 *)pasynInterface_->pinterface;
-    status = pasynInt32SyncIO->connect(portName, addr, &pasynUserSyncIO_, drvInfo);
 }
 
