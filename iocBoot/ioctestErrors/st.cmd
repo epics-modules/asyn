315f62019ef2e0598f9905d838f40a5a82af45e4
Mark Rivers <MarkRivers@users.noreply.github.com>
2015-01-07 21:42:05 +0000

    Added support for asyn:FIFO for optional ring buffer support on string and waveform records

--- iocBoot/ioctestErrors/st.cmd
+++ iocBoot/ioctestErrors/st.cmd
@@ -5,17 +5,20 @@ testErrorsConfigure("PORT1",)
 #asynSetTraceMask("PORT1",0,0xff)
 asynSetTraceIOMask("PORT1",0,0x2)
 
-### Use periodic scanning and normal timestamp (TSE=0)
-#dbLoadRecords("../../db/testErrors.db","P=testErrors:,PORT=PORT1,ADDR=0,TIMEOUT=1,TSE=0,SCAN=2 second")
+### Use periodic scanning and normal timestamp (TSE=0) and no ring buffer on string and waveform records
+#dbLoadRecords("../../db/testErrors.db","P=testErrors:,PORT=PORT1,ADDR=0,TIMEOUT=1,TSE=0,SCAN=2 second",FIFO=0")
 
-### Use I/O Intr scanning and normal timestamp (TSE=0)
-#dbLoadRecords("../../db/testErrors.db","P=testErrors:,PORT=PORT1,ADDR=0,TIMEOUT=1,TSE=0,SCAN=I/O Intr")
+### Use I/O Intr scanning and normal timestamp (TSE=0) and no ring buffer on string and waveform records
+#dbLoadRecords("../../db/testErrors.db","P=testErrors:,PORT=PORT1,ADDR=0,TIMEOUT=1,TSE=0,SCAN=I/O Intr,FIFO=0")
 
-### Use periodic scanning and timestamp from device support (TSE=-2)
-#dbLoadRecords("../../db/testErrors.db","P=testErrors:,PORT=PORT1,ADDR=0,TIMEOUT=1,TSE=-2,SCAN=2 second")
+### Use periodic scanning and timestamp from device support (TSE=-2) and no ring buffer on string and waveform records
+#dbLoadRecords("../../db/testErrors.db","P=testErrors:,PORT=PORT1,ADDR=0,TIMEOUT=1,TSE=-2,SCAN=2 second,FIFO=0")
 
-### Use I/O Intr scanning and timestamp from device support (TSE=-2)
-dbLoadRecords("../../db/testErrors.db","P=testErrors:,PORT=PORT1,ADDR=0,TIMEOUT=1,TSE=-2,SCAN=I/O Intr")
+### Use I/O Intr scanning and timestamp from device support (TSE=-2) and no ring buffer on string and waveform records
+#dbLoadRecords("../../db/testErrors.db","P=testErrors:,PORT=PORT1,ADDR=0,TIMEOUT=1,TSE=-2,SCAN=I/O Intr,FIFO=0")
+
+### Use I/O Intr scanning and timestamp from device support (TSE=-2) and 5 element ring buffer on string and waveform records
+dbLoadRecords("../../db/testErrors.db","P=testErrors:,PORT=PORT1,ADDR=0,TIMEOUT=1,TSE=-2,SCAN=I/O Intr,FIFO=5")
 
 ### Use user-defined time stamp source by uncommenting this line
 asynRegisterTimeStampSource("PORT1", "myTimeStampSource")
