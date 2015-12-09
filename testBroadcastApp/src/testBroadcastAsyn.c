475a7d35f2c55fde7c53a6b31f634fd753c34d33
Mark Rivers <rivers@cars.uchicago.edu>
2015-12-09 10:11:49 -0600

    Send broadcast in a loop to test if tty->farAddr address is changing

--- testBroadcastApp/src/testBroadcastAsyn.c
+++ testBroadcastApp/src/testBroadcastAsyn.c
@@ -20,13 +20,14 @@
 #include <asynShellCommands.h>
 
 #define PORT_NAME "PORT"
-
+#define NLOOPS 5
 int main(int argc, char *argv[]) {
 
     char buffer[256];
     asynStatus status;
     size_t nwrite, nread;
     int eomReason;
+    int i;
     asynUser *pasynUser;
     
     status = (asynStatus)drvAsynIPPortConfigure(PORT_NAME, "164.54.160.255:37747 UDP*", 0, 0, 0);
@@ -42,14 +43,15 @@ int main(int argc, char *argv[]) {
     status = pasynOctetSyncIO->connect(PORT_NAME, 0, &pasynUser, NULL);
     printf("Connected to UDP port, status=%d\n", status);
 
-    /* Send the broadcast message */
-    status = pasynOctetSyncIO->write(pasynUser, "i\n", 2, 1.0, &nwrite);
-    printf("Wrote to UDP port, status=%d, nwrite=%d\n", status, (int)nwrite);
-    
-    /* Read the first response */
-    status = pasynOctetSyncIO->read(pasynUser, buffer, sizeof(buffer), 0.1, &nread, &eomReason);
-    printf("Read from UDP port, status=%d, nread=%d, buffer=\n%s\n\n", status, (int)nread, buffer);
-    
+    for (i=0; i<NLOOPS; i++) {
+        /* Send the broadcast message */
+        status = pasynOctetSyncIO->write(pasynUser, "i\n", 2, 1.0, &nwrite);
+        printf("Wrote to UDP port, status=%d, nwrite=%d\n", status, (int)nwrite);
+
+        /* Read the first response */
+        status = pasynOctetSyncIO->read(pasynUser, buffer, sizeof(buffer), 0.1, &nread, &eomReason);
+        printf("Read from UDP port, status=%d, nread=%d, buffer=\n%s\n\n", status, (int)nread, buffer);
+    }
     asynReport(10, PORT_NAME);
 
 #ifdef linux
