533265251f9995fd937d4c6c9cd630d8ddd4c134
Mark Rivers <rivers@cars.uchicago.edu>
2016-08-16 17:58:36 -0500

    Remove definition of ioctl, no longer needed; prevent multiple inclusion

--- asyn/drvAsynSerial/os/default/serial_rs485.h
+++ asyn/drvAsynSerial/os/default/serial_rs485.h
@@ -1,3 +1,6 @@
+#ifndef SERIAL_RS485
+#define SERIAL_RS485
+
 #define SER_RS485_ENABLED         0x1
 #define SER_RS485_RTS_ON_SEND     0x2
 #define SER_RS485_RTS_AFTER_SEND  0x4
@@ -11,6 +14,4 @@ struct serial_rs485 {
   unsigned int delay_rts_after_send;
 };
 
-int ioctl( int fd, unsigned long request, void* ptr ) {
-  return 0;
-}
+#endif
