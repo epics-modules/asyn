b77b43d1ff751f7e1be258d9ca2be92a7017df68
Mark Rivers <rivers@cars.uchicago.edu>
2016-08-21 14:18:58 -0500

    Simplified, don't define serial_rs485 structure, not needed

--- asyn/drvAsynSerial/os/Linux/serial_rs485.h
+++ asyn/drvAsynSerial/os/Linux/serial_rs485.h
@@ -5,22 +5,13 @@
 #include <linux/serial.h>
 #include <linux/version.h>
 
-#define TIOCGRS485      0x542E
-#define TIOCSRS485      0x542F
 
 /* Older Linux systems don't support RS485. 
  * The features we need were introduced in 2.6.35 */
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,35)
   #define ASYN_RS485_SUPPORTED      1
-#else
-  #define SER_RS485_ENABLED         0x1
-  #define SER_RS485_RTS_ON_SEND     0x2
-  #define SER_RS485_RTS_AFTER_SEND  0x4
-  struct serial_rs485 {
-    int flags;
-    unsigned int delay_rts_before_send;
-    unsigned int delay_rts_after_send;
-  };
+  #define TIOCGRS485                0x542E
+  #define TIOCSRS485                0x542F
 #endif
 
 #endif
