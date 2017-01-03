639330c4731caceef06882ef1acb704c1d9114a8
Mark Rivers <rivers@cars.uchicago.edu>
2017-01-03 13:12:18 -0600

    Changed data type for string parameters from char * to std::string *

--- asyn/asynPortDriver/paramVal.cpp
+++ asyn/asynPortDriver/paramVal.cpp
@@ -220,18 +220,16 @@ double paramVal::getDouble()
 /** Sets the value for a string in the parameter library.
   * \param[out] value Address of value to set.
   * \return Returns asynParamBadIndex if the index is not valid or asynParamWrongType if the parameter type is not asynParamOctet. */
-void paramVal::setString(const char *value)
+void paramVal::setString(const std::string value)
 {
     if (type != asynParamOctet)
         throw ParamValWrongType("paramVal::setString can only handle asynParamOctet");
-    if (value == NULL)
-        throw ParamValWrongType("paramVal::setString can only handle non-NULL values");
-    if (!isDefined() || (strcmp(data.sval, value)))
+    if (!isDefined() || (*data.sval != value))
     {
         setDefined(true);
-        if (data.sval != NULL)
-            free(data.sval);
-        data.sval = epicsStrDup(value);
+        if (data.sval == 0) 
+            data.sval = new std::string();
+        *data.sval = value;
         setValueChanged();
     }
 }
@@ -240,13 +238,13 @@ void paramVal::setString(const char *value)
   * \throws ParamValWrongType if type is not asynParamOctet
   * \throws paramValNotDefined if the value is not defined
   */
-char* paramVal::getString()
+std::string paramVal::getString()
 {
     if (type != asynParamOctet)
         throw ParamValWrongType("paramVal::getString can only handle asynParamOctet");
     if (!isDefined())
-        throw ParamValNotDefined("paramVal::geString value not defined");
-    return data.sval;
+        throw ParamValNotDefined("paramVal::getString value not defined");
+    return *data.sval;
 }
 
 void paramVal::report(int id, FILE *fp, int details)
@@ -275,7 +273,7 @@ void paramVal::report(int id, FILE *fp, int details)
             break;
         case asynParamOctet:
             if (isDefined())
-                fprintf(fp, "Parameter %d type=string, name=%s, value=%s, status=%d\n", id, getName(), getString(), getStatus());
+                fprintf(fp, "Parameter %d type=string, name=%s, value=%s, status=%d\n", id, getName(), getString().c_str(), getStatus());
             else
                 fprintf(fp, "Parameter %d type=string, name=%s, value is undefined\n", id, getName());
             break;
