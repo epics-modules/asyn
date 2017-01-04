e9773cdf2aeee199dfd84e89088b4991ee7dc4e0
Mark Rivers <rivers@cars.uchicago.edu>
2017-01-04 15:35:49 -0600

    Remove definitions of errors because paramErrors.h is now installed

--- asyn/asynPortDriver/unittest/asynPortDriverTest.cpp
+++ asyn/asynPortDriver/unittest/asynPortDriverTest.cpp
@@ -20,13 +20,6 @@
 extern "C" int interruptAccept;
 int interruptAccept=1;
 
-/* paramErrors.h isn't installed */
-#define asynParamAlreadyExists (asynStatus)(asynDisabled + 1)
-#define asynParamNotFound      (asynStatus)(asynDisabled + 2)
-#define asynParamWrongType     (asynStatus)(asynDisabled + 3)
-#define asynParamBadIndex      (asynStatus)(asynDisabled + 4)
-#define asynParamUndefined     (asynStatus)(asynDisabled + 5)
-
 namespace {
 
 typedef epicsGuard<asynPortDriver> Guard;
