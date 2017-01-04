644528883122611e567ee61b9bd3dc5c7df74bbf
Mark Rivers <rivers@cars.uchicago.edu>
2017-01-04 15:16:18 -0600

    Moved sval out of union; added const qualifiers; pass by reference

--- asyn/asynPortDriver/paramVal.h
+++ asyn/asynPortDriver/paramVal.h
@@ -33,8 +33,8 @@ public:
     epicsUInt32 getUInt32(epicsUInt32 valueMask);
     void setDouble(epicsFloat64 value);
     epicsFloat64 getDouble();
-    void setString(std::string value);
-    std::string getString();
+    void setString(const std::string& value);
+    const std::string& getString();
     void report(int id, FILE *fp, int details);
     const char* getTypeName();
     asynParamType type; /**< Parameter data type */
@@ -50,13 +50,13 @@ protected:
     bool valueDefined;
     bool valueChanged;
     char *name;         /**< Parameter name */
+    std::string sval;
     /** Union for parameter value */
     union
     {
         epicsInt32   ival;
         epicsUInt32  uival;
         epicsFloat64 dval;
-        std::string  *sval;
         epicsInt8    *pi8;
         epicsInt16   *pi16;
         epicsInt32   *pi32;
