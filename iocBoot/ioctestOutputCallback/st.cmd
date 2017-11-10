fb1c7f15a83933d2873e317b1a542f655fbb0666
Mark Rivers <rivers@cars.uchicago.edu>
2017-11-10 12:51:44 -0600

    New file

---
+++ iocBoot/ioctestOutputCallback/st.cmd
@@ -0,0 +1,16 @@
+dbLoadDatabase("../../dbd/testOutputCallback.dbd")
+testOutputCallback_registerRecordDeviceDriver(pdbbase)
+
+# Arguments: Portname, canBlock, numCallbacks
+testOutputCallbackConfigure("PORT1", 1, 1)
+#asynSetTraceMask("PORT1",0,0xff)
+asynSetTraceIOMask("PORT1",0,0x2)
+
+### Use PINI=NO on output records and SCAN=I/O Intr on input records
+dbLoadRecords("../../db/testOutputCallback.db","P=testOutputCallback:,PORT=PORT1,ADDR=0,TIMEOUT=1,PINI=NO")
+
+dbLoadRecords("../../db/asynRecord.db","P=testOutputCallback:,R=asyn1,PORT=PORT1,ADDR=0,OMAX=80,IMAX=80")
+
+iocInit()
+
+
