59ff6a7c3f9a5ea2f86fb0d27b12ce6fe4485196
Mark Rivers <rivers@cars.uchicago.edu>
2020-09-01 10:51:18 -0500

    Added fix to allow it to work on base 3.14

--- asyn/asynDriver/asynAPI.h
+++ asyn/asynDriver/asynAPI.h
@@ -1,7 +1,21 @@
-/* This is a generated file, do not edit! */
-
 #ifndef INC_asynAPI_H
 #define INC_asynAPI_H
+
+#include <epicsVersion.h>
+
+#ifndef VERSION_INT
+//! Construct version number constant.
+#  define VERSION_INT(V,R,M,P) ( ((V)<<24) | ((R)<<16) | ((M)<<8) | (P))
+#endif
+
+#ifndef EPICS_VERSION_INT
+#  define EPICS_VERSION_INT VERSION_INT(EPICS_VERSION, EPICS_REVISION, EPICS_MODIFICATION, EPICS_PATCH_LEVEL)
+#endif
+
+#if defined(_WIN32) && EPICS_VERSION_INT<VERSION_INT(3,15,0,0) && !defined(EPICS_DLL_NO)
+#    define EPICS_BUILD_DLL
+#    define EPICS_CALL_DLL
+#endif
 
 #if defined(_WIN32) || defined(__CYGWIN__)
 
