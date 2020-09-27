86f644f63fb917b9e69648340528a1e07e5957ae
Mark Rivers <rivers@cars.uchicago.edu>
2020-09-27 08:59:24 -0500

    Replaced auto_ptr with unique_ptr; requires C++11

--- asyn/asynPortDriver/asynPortDriver.cpp
+++ asyn/asynPortDriver/asynPortDriver.cpp
@@ -129,7 +129,7 @@ asynStatus paramList::createParam(const char *name, asynParamType type, int *ind
 
     if (this->findParam(name, index) == asynSuccess) return asynParamAlreadyExists;
 
-    std::auto_ptr<paramVal> param(new paramVal(name, type));
+    std::unique_ptr<paramVal> param(new paramVal(name, type));
 
     vals.push_back(param.get());
     flags.reserve(vals.size());
