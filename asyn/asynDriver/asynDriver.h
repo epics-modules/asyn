8160e4d7c9bbf45dd5f6228fd0f70ae4c9c27dae
Mark Rivers <rivers@cars.uchicago.edu>
2021-05-10 10:55:23 -0500

    Change version to 4-42-0

--- asyn/asynDriver/asynDriver.h
+++ asyn/asynDriver/asynDriver.h
@@ -24,7 +24,7 @@
 /* Version number names similar to those provide by base
  * These macros are always numeric */
 #define ASYN_VERSION       4
-#define ASYN_REVISION     41
+#define ASYN_REVISION     42
 #define ASYN_MODIFICATION  0
 
 #ifndef EPICS_VERSION_INT
