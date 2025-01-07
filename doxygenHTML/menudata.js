c1f402bced5bb9db4dd1d65c3d3a63d94e04521b
MarkRivers <MarkRivers@users.noreply.github.com>
2025-01-07 22:29:55 +0000

    deploy: 0ed37b298307f3c32718bd6b28fcdf4bd459bacf

--- doxygenHTML/menudata.js
+++ doxygenHTML/menudata.js
@@ -75,7 +75,7 @@ var menudata={children:[
 {text:"t",url:"functions_vars.html#index_t"},
 {text:"u",url:"functions_vars.html#index_u"},
 {text:"v",url:"functions_vars.html#index_v"}]},
-{text:"Related Functions",url:"functions_rela.html"}]}]},
+{text:"Related Symbols",url:"functions_rela.html"}]}]},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
 {text:"File Members",url:"globals.html",children:[
